3a6bd9f42bafac9777615252f41c95fed8193b3f
Andrew Johnson <ajohnson.uva@gmail.com>
2018-11-26 15:13:07 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
+  i = 0
+  boolean_array = Array.new
+  while i < collection.length
+    boolean_array << yield(collection[i])
+    i += 1
+  end
 
+  !(boolean_array.include?(false))
 end
